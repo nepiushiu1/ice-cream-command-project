942d6765078b32361038b3bcf0f06d1dac6c237c
Ivan Roschin <ivan.roschin86@gmail.com>
2022-06-28 08:23:19 +0300

    modal-contact

---
+++ src/js/modal-contacts.js
@@ -0,0 +1,14 @@
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-contact-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-contact-close]'),
+//     modalcontact: document.querySelector('[data-modal-contact]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modalcontact.classList.toggle('is-hide');
+//   }
+// })();
