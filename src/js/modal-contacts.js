3ecd106051ddd88b38dca911384c31ee08d8578b
Ivan Roschin <ivan.roschin86@gmail.com>
2022-06-28 09:21:29 +0300

    add modal

--- src/js/modal-contacts.js
+++ src/js/modal-contacts.js
@@ -1,14 +1,14 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[data-modal-contact-open]'),
-//     closeModalBtn: document.querySelector('[data-modal-contact-close]'),
-//     modalcontact: document.querySelector('[data-modal-contact]'),
-//   };
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-contact-open]'),
+    closeModalBtn: document.querySelector('[data-modal-contact-close]'),
+    modalcontact: document.querySelector('[data-modal-contact]'),
+  };
 
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-//   function toggleModal() {
-//     refs.modalcontact.classList.toggle('is-hide');
-//   }
-// })();
+  function toggleModal() {
+    refs.modalcontact.classList.toggle('is-hidden');
+  }
+})();
