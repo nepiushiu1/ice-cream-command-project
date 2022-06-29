0a0386d5ab1578c1289c9fa2aaf2969cf93a60f7
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-06-29 16:08:58 +0300

    Перевел меню

--- src/js/modal_buy-now.js
+++ src/js/modal_buy-now.js
@@ -1,14 +1,25 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
+    // openModal_mobBtn: document.querySelector('[data-modal__mob-open]'),
+
     closeModalBtn: document.querySelector('[data-modal-close]'),
+    // closeModal_mobBtn: document.querySelector('[data-modal__mob-close]'),
+
     modal: document.querySelector('[data-modal]'),
+    // modal_mob: document.querySelector('[data-modal]'),
+
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
+  // refs.openModal_mobBtn.addEventListener('click', toggleModal);
+  // refs.closeModal_mobBtn.addEventListener('click', toggleModal);
+
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    // refs.modal_mob.classList.toggle('is-hidden');
+
   }
 })();
